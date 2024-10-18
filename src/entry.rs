479e5d3b496817fb73caf7b7e02bcee75b7fe37e
samatbk <samatbk@proton.me>
2024-10-18 11:16:53 +0300

    feat: print permissions of entry

--- src/entry.rs
+++ src/entry.rs
@@ -3,7 +3,11 @@ use crossterm::{
     queue,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
 };
-use std::{fs::Metadata, io::Cursor, os::unix::fs::MetadataExt};
+use std::{
+    fs::Metadata,
+    io::Cursor,
+    os::unix::fs::{MetadataExt, PermissionsExt},
+};
 
 #[derive(Clone)]
 pub struct Entry {
@@ -20,18 +24,35 @@ impl Entry {
             _ => (1_000_000_000.0, "gb"),
         };
 
+        let mode = self.metadata.permissions().mode();
+        let perms = format!(
+            "{}{}{}{}{}{}{}{}{}{} ",
+            if self.metadata.is_dir() { 'd' } else { '-' },
+            if mode & 0o400 != 0 { 'r' } else { '-' }, // User read
+            if mode & 0o200 != 0 { 'w' } else { '-' }, // User write
+            if mode & 0o100 != 0 { 'x' } else { '-' }, // User execute
+            if mode & 0o040 != 0 { 'r' } else { '-' }, // Group read
+            if mode & 0o020 != 0 { 'w' } else { '-' }, // Group write
+            if mode & 0o010 != 0 { 'x' } else { '-' }, // Group execute
+            if mode & 0o004 != 0 { 'r' } else { '-' }, // Others read
+            if mode & 0o002 != 0 { 'w' } else { '-' }, // Others write
+            if mode & 0o001 != 0 { 'x' } else { '-' }, // Others execute
+        );
+
         queue!(
             buffer,
-            SetForegroundColor(Color::Yellow),
             if highlight {
                 SetBackgroundColor(Color::DarkGrey)
             } else {
                 SetBackgroundColor(Color::Reset)
             },
+            SetForegroundColor(Color::Blue),
+            Print(perms),
+            SetForegroundColor(Color::Yellow),
             Print(format!(
                 "{: <8}",
                 if self.metadata.is_dir() {
-                    "dir".to_string()
+                    "-".to_string()
                 } else {
                     format!("{:.1}{}", self.metadata.size() as f32 / divisor, symbol)
                 }
@@ -41,7 +62,7 @@ impl Entry {
             } else {
                 SetForegroundColor(Color::Reset)
             },
-            Print(format!(" {: >32}\r\n", self.name)),
+            Print(format!(" {: <32}\r\n", self.name)),
             ResetColor
         )?;
 
